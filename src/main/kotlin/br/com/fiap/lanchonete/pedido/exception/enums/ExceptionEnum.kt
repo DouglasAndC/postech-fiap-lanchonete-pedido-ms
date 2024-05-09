10ad0b7abd774775d55de0119d3b40056f1b128b
DougAndC <sousaandradedouglas@gmail.com>
2024-05-08 21:41:03 -0300

    issue sonar Maintainability kotlin:S6517

--- src/main/kotlin/br/com/fiap/lanchonete/pedido/exception/enums/ExceptionEnum.kt
+++ src/main/kotlin/br/com/fiap/lanchonete/pedido/exception/enums/ExceptionEnum.kt
@@ -2,6 +2,6 @@ package br.com.fiap.lanchonete.pedido.exception.enums
 
 import br.com.fiap.lanchonete.pedido.exception.dto.ResponseErrorDto
 
-interface ExceptionEnum {
+fun interface ExceptionEnum {
     fun getResponseError(): ResponseErrorDto
 }
